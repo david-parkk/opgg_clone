3798b33352f6ab6f07e719e5cf8ab6e93e631c13
david-parkk <ji0513ji@naver.com>
2023-08-04 23:33:21 +0900

    init

---
+++ src/main/java/davidparkk/demo/SleeplessBoysApplication.java
@@ -0,0 +1,13 @@
+package davidparkk.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SleeplessBoysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SleeplessBoysApplication.class, args);
+	}
+
+}
---
+++ src/test/java/davidparkk/demo/SleeplessBoysApplicationTests.java
@@ -0,0 +1,13 @@
+package davidparkk.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SleeplessBoysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
